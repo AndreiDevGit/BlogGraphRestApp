92378052abc74cb947f5c3b3ae9a39e474a572c0
AndreiDev <a.v.babici@gmail.com>
2024-08-29 14:45:46 +0300

    Add user model + routes + register in middleware

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const { body } = require('express-validator')
+
+const User = require('../models/user')
+
+
+const router = express.Router()
+
+router.put()
+
+module.exports = router
