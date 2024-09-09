fd1a76fe0fee5802fe5d0204c6f3fd4223781909
AndreiDev <a.v.babici@gmail.com>
2024-09-10 00:50:57 +0300

    ini GraphQL + Defining a Mutation Schema + Adding a Mutation Resolver + user validation

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const multer = require('multer')
+const graphqlHttp = require('express-graphql').graphqlHTTP
+
+const graphqlSchema = require('./graphql/schema')
+const graphqlResolver = require('./graphql/resolvers')
 
 
 const app = express()
@@ -45,6 +49,15 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   next()
 })
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true
+  })
+)
 
 
 app.use((error, req, res, next) => {
